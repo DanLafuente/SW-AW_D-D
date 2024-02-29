908a7ab8c0e872b15ceb8ca5535e0071fd164fb6
Chuvakan <alrodr27@ucm.es>
2024-02-29 10:58:13 +0100

    W3C2

--- Miembros.php
+++ Miembros.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,10 +13,10 @@
         <!--Hoja-->
             <!--Lateral con referencias-->
             <h1>Miembros del Proyecto</h1>
-                <ol><li> <a href="#DanielLafuenteBazo">Daniel Lafuente Bazo <img src="img/daniLafu.png" height="64" width= "64"></a></li>
-                    <li> <a href="#HugoVivacioFernandez">Hugo Vivacio Fernandez <img src="img/hugouwu.png" height="64" width= "64"></a> </li>
-                    <li> <a href="#JaimeIsarMuñoz">Jaime Isar Muñoz <img src="img/jimmysaurio.png" height="64" width= "64"></a> </li>
-                    <li> <a href="#AlejandroRodriguezGiner">Alejandro Rodriguez Giner<img src="img/chuvak.png" height="64" width= "64"></a> </li>
+                <ol><li> <a href="#DanielLafuenteBazo">Daniel Lafuente Bazo <img src="img/daniLafu.png" alt="Pueblerino" height="64" width= "64"></a></li>
+                    <li> <a href="#HugoVivacioFernandez">Hugo Vivacio Fernandez <img src="img/hugouwu.png" alt="Furro" height="64" width= "64"></a> </li>
+                    <li> <a href="#JaimeIsarMuñoz">Jaime Isar Muñoz <img src="img/jimmysaurio.png" alt="Estegosaurio" height="64" width= "64"></a> </li>
+                    <li> <a href="#AlejandroRodriguezGiner">Alejandro Rodriguez Giner<img src="img/chuvak.png" alt="Mascota" height="64" width= "64"></a> </li>
 
                 </ol>
             <!--Lista de miembros del equipo de desarrollo-->
@@ -44,19 +44,27 @@
                         </dl>
                     </li>
                     <li id="JaimeIsarMuñoz">Jaime Isar Muñoz
-                        <dl><dt>Nombre: Jaime Isar Muñoz</dt>
-                        <dt>Correo de contacto: jaimisar@ucm.es</dt>
-                        <dt>Imagen de Jaime: <img src="img/jimmy.jpg" height="516" width="300"></dt>
-                        <dt>Historia: Tambien conocido como Jimmy, es un ser de luz que se dedica ha hacer el mal. Nos hace feliz tenerlo, pero por favor,
-                             no le des nada inflamabe. Mensaje: "Erbisiho is coming for your weakness."</dt>
+                        <dl><dt>Nombre:</dt>
+                            <dd>Jaime Isar Muñoz</dd>
+                        <dt>Correo de contacto:</dt>
+                            <dd> jaimisar@ucm.es</dd>
+                        <dt>Imagen de Jaime:</dt>
+                            <dd><img src="img/jimmy.jpg" alt="Estegosaurio" height="516" width="300"></dd>
+                        <dt>Historia:</dt>
+                            <dd>Tambien conocido como Jimmy, es un ser de luz que se dedica ha hacer el mal. Nos hace feliz tenerlo, pero por favor,
+                             no le des nada inflamabe. Mensaje: "Erbisiho is coming for your weakness."</dd>
                         </dl>
                     </li>
                     <li id="AlejandroRodriguezGiner">Alejandro Rodriguez Giner
-                        <dl><dt>Nombre: Alejandro Rodriguez Giner</dt>
-                        <dt>Correo de contacto: alrodr27@ucm.es</dt>
-                        <dt>Imagen de Alejandro: <img src="img/alex.jpg" height="400" width="300"></dt>
-                        <dt>Historia: Saludad a la mascota, tambien conocida como Chuvakan. Es tan mono que se cree artista cuando ha hecho gran parte del diseño de esta página.
-                            Mensaje: "No me mireis que me estoy cambiando."</dt>
+                        <dl><dt>Nombre:</dt>
+                            <dd>Alejandro Rodriguez Giner</dd>
+                        <dt>Correo de contacto:</dt>
+                            <dd>alrodr27@ucm.es</dd>
+                        <dt>Imagen de Alejandro:</dt>
+                            <dd> <img src="img/alex.jpg" alt="Mascota" height="400" width="300"></dd>
+                        <dt>Historia:</dt>
+                            <dd>Saludad a la mascota, tambien conocida como Chuvakan. Es tan mono que se cree artista cuando ha hecho gran parte del diseño de esta página.
+                            Mensaje: "No me mireis que me estoy cambiando."</dd>
                         </dl>
                     </li>
                 </ol>
